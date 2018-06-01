3ae954876ff677aaf05f9de328347ce5cb719728
Eben Zhang <zhangyingneng@gmail.com>
2018-06-01 18:46:26 +0800

    wip

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import withRoot from '../containers/WithRoot';
+import { Typography } from '@material-ui/core';
 
-let styles = require('./Home.scss');
+const styles = require('./Home.scss');
 
-export default class Home extends React.Component {
+class Home extends React.Component {
   render() {
     return (
       <div>
+        <Typography variant="display1" gutterBottom>
+          Material-UI
+        </Typography>
         <div className={styles.container} data-tid="container">
           <h2>Home</h2>
           <Link to="/counter">to Counter</Link>
@@ -15,3 +20,5 @@ export default class Home extends React.Component {
     );
   }
 }
+
+export default withRoot(Home);
